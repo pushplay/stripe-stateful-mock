d04498feb42d8fab0b04c212ab9f788fb3fde47e
jeff <jeff@giftbit.com>
2019-08-22 09:46:03 -0700

    Get some auth action.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import expressBasicAuth from 'express-basic-auth';
 import {routes} from './routes';
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressBasicAuth({
+    authorizer: (username: string, password: string) => /^sk_test_/.test(username),
+    unauthorizedResponse: (req: express.Request) => ({
+        error: {
+            message: "Invalid API Key provided: *************************",
+            type: "invalid_request_error"
+        }
+    })
+}));
 app.use('/', routes);
 app.listen(+port, () => {
     console.log(`Server started on port ${port}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 
 const routes = Router();
+
 routes.get('/', (req, res) => {
+    return res.status(200).json({
+        message: "Hello world",
+    });
+});
+
+routes.post('/v1/charges', (req, res) => {
+    console.log("user=", (req as any).user);
     return res.status(200).json({
         message: "Hello world",
     });
